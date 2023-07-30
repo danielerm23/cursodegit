e84003916ba0a7bfd2fe16656bf5ac474f7d6040
danielerm23 <danielerm006@gmail.com>
2023-07-30 23:29:05 +0200

    JODER NO ME DEJAN CAMBIAR

--- mpp.js
+++ mpp.js
@@ -3,4 +3,17 @@ frio.addEventListener('click', funcionRespuesta)
 
 function funcionRespuesta(){
     alert ("soplandote")
+    
+    let con=0
+    while (con==0){
+        let respuesta=prompt ("te gustó ? 😏 si o no?")
+        if (respuesta=="si"){
+            alert("que rico que te haya gustado 😏")
+            con=1
+        } else if (respuesta == "no"){
+            alert("ni modo que me importara gonorrea pirob@")
+            con=1
+        }
+        
+    }
 }
